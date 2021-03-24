f18138a177f805237f081cbe71c2a3f8016435b0
bastos-01 <p.bastos112@gmail.com>
2021-03-24 17:23:30 +0000

    all done

--- lab2/P2Geocoding/src/test/java/integration/AddressResolverIT.java
+++ lab2/P2Geocoding/src/test/java/integration/AddressResolverIT.java
@@ -3,36 +3,34 @@ package integration;
 import connection.TqsBasicHttpClient;
 import geocoding.Address;
 import geocoding.AddressResolver;
-import org.json.simple.parser.ParseException;
-import org.junit.jupiter.api.BeforeEach;
+
+
+import connection.TqsBasicHttpClient;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.text.ParseException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AddressResolverIT {
 
+    private AddressResolver resolver = new AddressResolver(new TqsBasicHttpClient());
 
-    @BeforeEach
-    public void init(){
+    @Test
+    void whenGoodAlboiGps_returnAddress() throws ParseException, IOException, URISyntaxException, org.json.simple.parser.ParseException {
+        //test
+        Address response = resolver.findAddressForLocation(40.640661, -8.656688);
+        Address real_address = new Address( "Cais do Alboi", "Glória e Vera Cruz", "Centro", "3800-246", null);
+
+        assertThat(response.toString(), is(real_address.toString()));
     }
 
     @Test
-    public void whenGoodCoordidates_returnAddress() throws IOException, URISyntaxException, ParseException {
-        //todo
-
-        //Address result = resolver.findAddressForLocation(40.640661, -8.656688);
-        //assertEquals( result, new Address( "Cais do Alboi", "Glória e Vera Cruz", "Centro", "3800-246", null) );
-
+    public void whenBadCoordidates_throwBadArrayindex() throws IOException, URISyntaxException, ParseException {
+        assertThrows(IndexOutOfBoundsException.class, () -> resolver.findAddressForLocation(40.640661, -8.656688));
     }
-
-    @Test
-    public void whenBadCoordidates_trhowBadArrayindex() throws IOException, URISyntaxException, ParseException {
-        // todo
-
-    }
-
 }
