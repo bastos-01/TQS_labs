7038b2269419365789d63739f4bfac38005a0498
bastos-01 <p.bastos112@gmail.com>
2021-04-05 18:00:14 +0100

    1 done

---
+++ lab3/src/test/java/SampleTest.java
@@ -0,0 +1,38 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+class SampleTest {
+
+    WebDriver browser;
+
+    @BeforeEach
+    void setUp(){
+        // System.setProperty("webdriver.chrome.driver", "/where/you/put/chromedriver");
+        browser = new FirefoxDriver();
+    }
+
+    @Test
+    public void site_header_is_on_home_page() {
+        browser.get("https://www.saucelabs.com");
+        WebElement href = browser.findElement(By.xpath("//a[@href='https://accounts.saucelabs.com/']"));
+        assertTrue((href.isDisplayed()));
+
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        browser.close();
+    }
+
+
+
+}
