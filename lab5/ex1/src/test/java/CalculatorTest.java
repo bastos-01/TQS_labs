e23e9f1725de2985330ef9633f0544333edefdd2
bastos-01 <p.bastos112@gmail.com>
2021-04-19 18:00:12 +0100

    lab5 ex 1

---
+++ lab5/ex1/src/test/java/CalculatorTest.java
@@ -0,0 +1,10 @@
+import io.cucumber.junit.platform.engine.Cucumber;
+
+@Cucumber
+public class CalculatorTest {
+
+    // See:
+    // https://github.com/cucumber/cucumber-jvm/issues/1149
+    // https://github.com/cucumber/cucumber-jvm/tree/master/junit-platform-engine
+
+}
